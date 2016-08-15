3e0e74b5624943ac780be675b4e4d52f448ff867
Steve Persch <steve.persch@pantheon.io>
2016-08-14 21:11:42 -0500

    Fixing clone

--- tests/circle-scripts/setup-d8-repo.sh
+++ tests/circle-scripts/setup-d8-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Bring the code down to Circle so that modules can be added via composer.
-git clone $(terminus site connection-info --field=git_url) drupal8
-cd drupal8
+git clone $(terminus site connection-info --field=git_url) $TERMINUS_SITE
+cd $TERMINUS_SITE
 git checkout -b $TERMINUS_ENV
