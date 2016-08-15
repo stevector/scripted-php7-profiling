e9861f25bf715851c2e11d1ec3dc2d24e52c17e2
Steve Persch <steve.persch@pantheon.io>
2016-08-14 21:26:57 -0500

    fix checkout of branch

--- tests/circle-scripts/setup-d8-repo.sh
+++ tests/circle-scripts/setup-d8-repo.sh
@@ -3,4 +3,4 @@
 # Bring the code down to Circle so that modules can be added via composer.
 git clone $(terminus site connection-info --field=git_url) $TERMINUS_SITE
 cd $TERMINUS_SITE
-git checkout -b $TERMINUS_ENV
+git checkout $TERMINUS_ENV
