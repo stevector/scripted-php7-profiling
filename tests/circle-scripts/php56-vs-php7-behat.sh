bb33f675feb27aed45ac528d4046c875d2bbd876
Steve Persch <stevector@users.noreply.github.com>
2016-09-08 18:17:17 -0500

    Installing LCache [ci skip]

--- tests/circle-scripts/php56-vs-php7-behat.sh
+++ tests/circle-scripts/php56-vs-php7-behat.sh
@@ -2,35 +2,35 @@
 
 set -ex
 
-terminus site clear-cache
-terminus drush "config-list"
-terminus drush help
-terminus drush pml
-terminus drush status
-sleep 60
-
 cd ${TERMINUS_SITE}
 
-echo "php_version: 5.6 " >> pantheon.yml
+echo "php_version: 7.0 " >> pantheon.yml
 
 git add pantheon.yml
-git commit -m 'Setting php 5.6'
+git commit -m 'Setting php 7, clean install of Drupal Core'
 git push
 
 sleep 30
+terminus drush "si -y"
+terminus site clear-cache
 
 cd ..
 ./execute-behat.sh
 
-
 cd ${TERMINUS_SITE}
 sleep 15
-echo "php_version: 7.0" >> pantheon.yml
+
+composer config repositories.drupal composer https://packages.drupal.org/8
+composer require drupal/lcache:1.x-dev
 
 git add pantheon.yml
-git commit -m 'Setting php 7.0'
+git commit -m 'Adding LCache'
 git push
 
 sleep 60
+terminus drush "si -y"
+terminus drush "en lcache -y"
+terminus site clear-cache
+
 cd ..
 ./execute-behat.sh
