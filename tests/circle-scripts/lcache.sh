58e286e124e0bbfe854aa9c29a64c9bef00d5577
Steve Persch <steve.persch@pantheon.io>
2016-09-08 23:32:25 -0500

    200 runs

--- tests/circle-scripts/lcache.sh
+++ tests/circle-scripts/lcache.sh
@@ -40,7 +40,7 @@ terminus site clear-cache
 
 
 cd ..
-for i in $(seq 2); do
+for i in $(seq 200); do
   echo "Peformance test pass $i with Core"
   ./../../vendor/bin/behat --config=../behat/behat-pantheon.yml ../behat/features/create-node-view-all-nodes.feature
 done
@@ -83,7 +83,7 @@ git push
 
 
 cd ..
-for i in $(seq 2); do
+for i in $(seq 200); do
   echo "Peformance test pass $i with LCache"
   ./../../vendor/bin/behat --config=../behat/behat-pantheon.yml ../behat/features/create-node-view-all-nodes.feature
 done
